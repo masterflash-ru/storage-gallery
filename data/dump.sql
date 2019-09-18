a55fbfdab2b85305f672193ebdf22501126b702e
Masterflash <sxq@yandex.ru>
2019-09-18 12:49:27 +0300

    Update dump.sql

--- data/dump.sql
+++ data/dump.sql
@@ -37,34 +37,6 @@ CREATE TABLE `storage_gallery` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='галерея в хранилище';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `storage_gallery`
---
-
-LOCK TABLES `storage_gallery` WRITE;
-/*!40000 ALTER TABLE `storage_gallery` DISABLE KEYS */;
-/*!40000 ALTER TABLE `storage_gallery` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8 */ ;
-/*!50003 SET character_set_results = utf8 */ ;
-/*!50003 SET collation_connection  = utf8_general_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'NO_AUTO_CREATE_USER' */ ;
-DELIMITER ;;
-/*!50003 CREATE*/ /*!50003 TRIGGER `storage_gallery_before_del_tr` BEFORE DELETE ON `storage_gallery`
-  FOR EACH ROW
-BEGIN
-update storage set todelete=1 where razdel=concat(OLD.razdel,'_',OLD.razdel_id) and id=OLD.id;
-END */;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
