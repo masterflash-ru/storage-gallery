1452e31423b454f865f25d0ae34ff6ddae8ebdbd
Masterflash <sxq@yandex.ru>
2020-05-19 09:59:46 +0300

    Update Version20201905163546.php

--- migrations/Version20201905163546.php
+++ migrations/Version20201905163546.php
@@ -15,7 +15,7 @@ class Version20201905163546 extends AbstractMigration implements MigrationInterf
         $table = new Ddl\CreateTable("storage_gallery");
         $table->addColumn(new Ddl\Column\Integer('id',false,null,["AUTO_INCREMENT"=>true]));
         $table->addColumn(new Ddl\Column\Char('razdel', 50,false,null,["COMMENT"=>"раздел, например, news"]));
-        $table->addColumn(new Ddl\Column\Integer('razdel_id', 50,false,null,["COMMENT"=>"ID раздела, например, 10"]));
+        $table->addColumn(new Ddl\Column\Integer('razdel_id',false,null,["COMMENT"=>"ID раздела, например, 10"]));
         $table->addColumn(new Ddl\Column\Integer('todelete',true,0,["COMMENT"=>"флаг что нужно удалить эти фото"]));
         $table->addColumn(new Ddl\Column\Char('storage_item_name',100,true,null,["COMMENT"=>"Имя эл-та хранилища обработки отдельных фото галереи"]));
         $table->addColumn(new Ddl\Column\Datetime('date_public', true,null,["COMMENT"=>"дата публикации"]));
@@ -28,7 +28,7 @@ class Version20201905163546 extends AbstractMigration implements MigrationInterf
             new Ddl\Constraint\PrimaryKey(['id'])
         );
         $table->addConstraint(
-            new Ddl\Constraint\Index(['razdel_id','razdel'])
+            new Ddl\Index\Index(['razdel_id','razdel'])
         );
         $table->addConstraint(
             new Ddl\Index\Index(['todelete'])
